c47a42d38f5107ade67e55f1ba6ca7bd8f419a0b
wcrowe <crowe.will@gmail.com>
2025-11-30 08:13:55 -0500

    main cli options

--- src/main.c
+++ src/main.c
@@ -7,9 +7,33 @@
 #include "file.h"
 #include "parse.h"
 
+
 void print_usage(char *argv[]) {
 }
 
-int main(int argc, char *argv[]) { 
-	
+
+int main(int argc, char *argv[]) {
+	int c = 0;
+
+	while ((c = getopt(argc, argv, "anf:")) != -1) {
+		switch (c) {
+		case 'a':
+				/* filename provided in optarg */
+				(void)optarg;
+				break;
+		case 'f':
+				/* filename provided in optarg */
+				(void)optarg;
+				break;
+			case 'n':
+				/* -n flag handling */
+				break;
+			default:
+				print_usage(argv);
+				exit(EXIT_FAILURE);
+		}
+	}
+
+	return 0;
+
 }
