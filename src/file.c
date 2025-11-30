99b8ee0429ddc31bf9a414715339283eba7c57c5
wcrowe <crowe.will@gmail.com>
2025-11-30 09:32:34 -0500

    file open and create

--- src/file.c
+++ src/file.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#include "file.h"
 #include "common.h"
-
+#include "file.h"
 
 int create_db_file(char *filename) {
-
+  int fd = open(filename, O_RDONLY);
+  if (fd != -1) {
+    printf("File already exists.\n");
+    close(fd);
+    return STATUS_ERROR;
+  }
+  fd = open(filename, O_RDWR | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+  if (fd == -1) {
+    perror("Error creating file");
+    return STATUS_ERROR;
+  }
+  return STATUS_SUCCESS;
 }
 
 int open_db_file(char *filename) {
-
+  int fd = open(filename, O_RDONLY | S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+  if (fd == -1) {
+    printf("File not found.\n");
+    return STATUS_ERROR;
+  }
+  return STATUS_SUCCESS;
 }
-
-
--- src/main.c
+++ src/main.c
@@ -9,11 +9,18 @@
 
 
 void print_usage(char *argv[]) {
+    printf("Usage: %s [-a filename] [-f filename] [-n]\n", argv[0]);
+    printf("\t -n : create a new db file\n");
+    printf("\t -a : specify an additional db filename\n");
+    printf("\t -f : specify the db filename\n");
 }
 
 
 int main(int argc, char *argv[]) {
+	bool newFile = false;
 	int c = 0;
+	char *filename = NULL;
+	int dbfd = -1;
 
 	while ((c = getopt(argc, argv, "anf:")) != -1) {
 		switch (c) {
@@ -23,10 +30,12 @@ int main(int argc, char *argv[]) {
 				break;
 		case 'f':
 				/* filename provided in optarg */
-				(void)optarg;
+                filename = optarg;
+			//	(void)optarg;
 				break;
 			case 'n':
 				/* -n flag handling */
+                newFile = true;
 				break;
 			default:
 				print_usage(argv);
@@ -34,6 +43,25 @@ int main(int argc, char *argv[]) {
 		}
 	}
 
-	return 0;
+    if (!newFile && filename == NULL) {
+        print_usage(argv);
+        exit(EXIT_SUCCESS);
+    }
+
+    if (newFile) {
+       dbfd = create_db_file(filename);
+       if (dbfd == STATUS_ERROR) {
+           exit(EXIT_FAILURE);
+       }
+        printf("Creating a new file...%d\n", newFile);
+    } else {
+        dbfd = open_db_file(filename);
+        if (dbfd == STATUS_ERROR) {
+            exit(EXIT_FAILURE);
+        }
+        printf("Opening existing file...%d\n",filename != NULL );
+    }
+
+	return STATUS_SUCCESS;
 
 }
