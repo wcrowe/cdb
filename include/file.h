5c103e6585effe738f9f54da2d700dbd9a0c0514
wcrowe <crowe.will@gmail.com>
2025-11-30 21:27:26 -0500

    access rights

--- include/file.h
+++ include/file.h
@@ -4,4 +4,7 @@
 int create_db_file(char *filename);
 int open_db_file(char *filename);
 
+#define PERM_644 (S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH)   // rw-r--r--
+#define PERM_755 (S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH)  // rwxr-xr-x
+#define PERM_600 (S_IRUSR|S_IWUSR)                    // rw-------
 #endif
