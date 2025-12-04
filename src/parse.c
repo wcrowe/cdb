6f8e620afbee01ce9a6cb20bb9a2649f7c090029
wcrowe <crowe.will@gmail.com>
2025-12-03 21:26:04 -0500

    ck

--- src/parse.c
+++ src/parse.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 #include <arpa/inet.h>
 #include "common.h"
 #include "parse.h"
 
 int create_db_header(struct dbheader_t **headerOut)
 {
-    struct dbheader_t *h = calloc(1, sizeof(struct dbheader_t));
+    struct dbheader_t *h = calloc(1, sizeof(*h));
     if (!h) return STATUS_ERROR;
 
     h->magic    = HEADER_MAGIC;
@@ -19,6 +19,88 @@ int create_db_header(struct dbheader_t **headerOut)
     h->filesize = sizeof(struct dbheader_t);
 
     *headerOut = h;
+    return STATUS_SUCCESS;
+}
+
+int validate_db_header(int fd, struct dbheader_t **headerOut)
+{
+    struct dbheader_t *h = calloc(1, sizeof(*h));
+    if (!h) return STATUS_ERROR;
+
+    lseek(fd, 0, SEEK_SET);
+    if (read(fd, h, sizeof(*h)) != sizeof(*h)) {
+        free(h);
+        return STATUS_ERROR;
+    }
+
+    h->magic    = ntohl(h->magic);
+    h->version  = ntohs(h->version);
+    h->count    = ntohs(h->count);
+    h->filesize = ntohl(h->filesize);
+
+    if (h->magic != HEADER_MAGIC || h->version != 1) {
+        free(h);
+        return STATUS_ERROR;
+    }
+
+    struct stat st;
+    if (fstat(fd, &st) == 0 && (off_t)h->filesize != st.st_size) {
+        free(h);
+        return STATUS_ERROR;
+    }
+
+    *headerOut = h;
+    return STATUS_SUCCESS;
+}
+
+int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut)
+{
+    if (dbhdr->count == 0) {
+        *employeesOut = NULL;
+        return STATUS_SUCCESS;
+    }
+
+    lseek(fd, sizeof(struct dbheader_t), SEEK_SET);
+    struct employee_t *emps = calloc(dbhdr->count, sizeof(struct employee_t));
+    if (!emps) return STATUS_ERROR;
+
+    if (read(fd, emps, dbhdr->count * sizeof(struct employee_t)) !=
+        dbhdr->count * sizeof(struct employee_t)) {
+        free(emps);
+        return STATUS_ERROR;
+    }
+
+    for (int i = 0; i < dbhdr->count; i++)
+        emps[i].hours = ntohl(emps[i].hours);
+
+    *employeesOut = emps;
+    return STATUS_SUCCESS;
+}
+
+int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees)
+{
+    dbhdr->filesize = sizeof(struct dbheader_t) + dbhdr->count * sizeof(struct employee_t);
+
+    struct dbheader_t net = *dbhdr;
+    net.magic    = htonl(net.magic);
+    net.version  = htons(net.version);
+    net.count    = htons(net.count);
+    net.filesize = htonl(net.filesize);
+
+    lseek(fd, 0, SEEK_SET);
+    if (write(fd, &net, sizeof(net)) != sizeof(net))
+        return STATUS_ERROR;
+
+    if (employees && dbhdr->count > 0) {
+        for (int i = 0; i < dbhdr->count; i++) {
+            struct employee_t e = employees[i];
+            e.hours = htonl(e.hours);
+            if (write(fd, &e, sizeof(e)) != sizeof(e))
+                return STATUS_ERROR;
+        }
+    }
+
+    fsync(fd);
     return STATUS_SUCCESS;
 }
 
@@ -38,9 +120,9 @@ int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees, char *
     *employees = new_mem;
 
     struct employee_t *e = &(*employees)[dbhdr->count];
-    strncpy(e->name,    name,  NAME_LEN-1);    e->name[NAME_LEN-1]    = '\0';
-    strncpy(e->address, addr,  ADDRESS_LEN-1); e->address[ADDRESS_LEN-1] = '\0';
-    e->hours = atoi(hours);
+    strncpy(e->name,    name,  255); e->name[255]    = '\0';
+    strncpy(e->address, addr,  255); e->address[255] = '\0';
+    e->hours = (unsigned int)atoi(hours);
 
     dbhdr->count++;
     return STATUS_SUCCESS;
@@ -48,8 +130,8 @@ int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees, char *
 
 void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees)
 {
-    if (!dbhdr || !employees || dbhdr->count == 0) {
-        printf("Database empty or not loaded.\n");
+    if (!dbhdr || dbhdr->count == 0) {
+        printf("No employees in database.\n");
         return;
     }
     printf("\n=== Employee Database (%d records) ===\n\n", dbhdr->count);
